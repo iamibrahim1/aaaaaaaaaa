b4dc33077bfdbd14dfb6937bbdb2a36edbe9b523
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-08-09 21:12:27 +0500

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**ᴊᴏɪɴ -->> [🍁 ᴄʜᴀɴɴᴇʟ 🍁](https://t.me/The_Happy_Hour_Hindi)**'
+        rep = '**ᴊᴏɪɴ -->> [🍁 ᴄʜᴀɴɴᴇʟ 🍁](https://t.me/digitalhub04)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
